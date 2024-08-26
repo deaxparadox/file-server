7d79a46e1ead2476b555714f3ad53df35347f9f3
deaxparadox <nitish136900kushwaha@gmail.com>
2024-08-26 12:24:17 +0530

    added env configuration support

--- fileserver/userve.py
+++ fileserver/userve.py
@@ -1,12 +1,31 @@
+import os
 import uvicorn
 
+
+# fastapi app
 from main import app
 
 
+# CONFIGURATIONS
+
+WORKERS = os.getenv("WORKERS")
+if not WORKERS:
+    WORKERS = 1
+else:
+    WORKERS = int(WORKERS)
+
+
+PORT = os.getenv("PORT")
+if not PORT:
+    PORT = 8000
+else:
+    PORT = int(PORT)
+    
+    
+    
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        port=9000,
-        workers=5,
-        
+        port=PORT,
+        workers=WORKERS,
     )
